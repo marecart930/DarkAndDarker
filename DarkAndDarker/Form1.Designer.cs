ff594fd664ceaa7ab4fd870002bc242e4ab6b62c
marecart930 <marecart930@SDS-L-991HC63>
2023-06-19 14:43:17 -0400

    Add project files.

---
+++ DarkAndDarker/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DarkAndDarker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.cordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 60;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Viner Hand ITC", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(39, 35);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(517, 78);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Dark and Darker 2D";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Transparent;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Viner Hand ITC", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(231, 262);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(145, 72);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_click);
+            // 
+            // cordLabel
+            // 
+            this.cordLabel.AutoSize = true;
+            this.cordLabel.BackColor = System.Drawing.Color.Transparent;
+            this.cordLabel.ForeColor = System.Drawing.Color.Lime;
+            this.cordLabel.Location = new System.Drawing.Point(58, 54);
+            this.cordLabel.Name = "cordLabel";
+            this.cordLabel.Size = new System.Drawing.Size(35, 13);
+            this.cordLabel.TabIndex = 2;
+            this.cordLabel.Text = "label1";
+            this.cordLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.Controls.Add(this.cordLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dark and Darker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label cordLabel;
+    }
+}
+
---
+++ DarkAndDarker/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace DarkAndDarker
+{
+    public partial class Form1 : Form
+    {
+
+        bool debugging = true;
+
+        //Hero variables
+        Rectangle hero = new Rectangle(200, 250, 30, 30);
+        int heroSpeed = 10;
+
+        //rooms
+        int room = 1;
+
+        Rectangle room1top = new Rectangle(50, 50, 500, 30);
+        Rectangle room1bottom = new Rectangle(50, 530, 500, 30);
+        Rectangle room1left = new Rectangle(50, 50, 30, 500);
+        Rectangle room1right = new Rectangle(520, 50, 30, 500);
+
+        Rectangle room2Hleft = new Rectangle(80, 245, 160, 30);
+
+
+        //knife
+        string knife = "up";
+
+        //Rectangle knife = new Rectangle(210, 250, 30, 30);
+
+        //enemys 
+
+
+        int room2;
+
+
+        //int score = 0;
+        int time = 1000;
+        int heroScore = 0;
+
+
+        bool leftDown = false;
+        bool rightDown = false;
+        bool upDown = false;
+        bool downDown = false;
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        Random randGen = new Random();
+        int randValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //draw hero
+            e.Graphics.FillRectangle(whiteBrush, hero);
+            e.Graphics.FillRectangle(whiteBrush, room1top);
+            e.Graphics.FillRectangle(whiteBrush, room1bottom);
+            e.Graphics.FillRectangle(whiteBrush, room1left);
+            e.Graphics.FillRectangle(whiteBrush, room1right);
+
+            if (room == 2)
+            {
+                e.Graphics.FillRectangle(whiteBrush, room2Hleft);
+            }
+
+            if (debugging == true)
+            {
+                cordLabel.Visible = true;
+                cordLabel.Text = $"X: {hero.X}\nY:{hero.Y}";
+            }
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+
+                case Keys.Up:
+                    upDown = false;
+                    break;
+                case Keys.Down:
+                    downDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+
+                case Keys.Up:
+                    upDown = true;
+                    break;
+                case Keys.Down:
+                    downDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            if (knife == "up")
+            {
+                Rectangle knifeUp = new Rectangle(hero.X, hero.Y, 5, 25);
+                g.FillRectangle(whiteBrush, knifeUp);
+            }
+            /*
+            else if (knife == "down")
+            {
+                Rectangle knifeDown = new Rectangle(hero.X + hero.Width, hero.Y - (hero.Height / 2), 7, 5);
+                g.FillRectangle(whiteBrush, knifeDown);
+            }
+            else if (knife == "left")
+            {
+                Rectangle knifeLeft = new Rectangle(hero.X + hero.Width, hero.Y + (hero.Height / 2), 7, 5);
+                g.FillRectangle(whiteBrush, knifeLeft);
+            }
+            else if (knife == "right")
+            {
+                Rectangle knifeRight = new Rectangle(hero.X + hero.Width, hero.Y + (hero.Height / 2), 7, 5);
+                g.FillRectangle(whiteBrush, knifeRight);
+            } 
+           */
+            //room = 2;
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        { 
+            switch (room)
+            {
+                case 1:
+                    //walls
+                    if (hero.X < 80)
+                    {
+                        hero.X += heroSpeed;
+                    }
+                    if (hero.X > 490)
+                    {
+                        hero.X -= heroSpeed;
+                    }
+                    if (hero.Y < 80)
+                    {
+                        hero.Y += heroSpeed;
+                    }
+                    if (hero.Y > 500)
+                    {
+                        hero.Y -= heroSpeed;
+                    }
+                    break;
+
+                case 2:
+                    //main walls           
+                    if (hero.X < 80)
+                    {
+                        hero.X += heroSpeed;
+                    }
+                    if (hero.X > 490)
+                    {
+                        hero.X -= heroSpeed;
+                    }
+                    if (hero.Y < 80)
+                    {
+                        hero.Y += heroSpeed;
+                    }
+                    if (hero.Y > 500)
+                    {
+                        hero.Y -= heroSpeed;
+                    }
+                    //secondary walls
+                    if (hero.Y < 270 && hero.X < 239 && hero.Y > 240)
+                    {
+                        hero.Y = 270;
+                    }
+                    if (hero.Y <= 230 && hero.X < 235 && hero.Y > 220)
+                    {
+                        hero.Y = 215;
+                    }
+                    break;
+            }
+
+
+            if (upDown == true && hero.Y > 0)
+            {
+                hero.Y -= heroSpeed;
+            }
+            if (downDown == true && hero.Y > 0)
+            {
+                hero.Y += heroSpeed;
+            }
+
+            if (wDown == true && hero.Y > 0)
+            {
+                hero.Y -= heroSpeed;
+            }
+            if (sDown == true && hero.Y > 0)
+            {
+                hero.Y += heroSpeed;
+            }
+
+            if (aDown == true && hero.X > 0)
+            {
+                hero.X -= heroSpeed;
+            }
+            if (dDown == true && hero.X > 0)
+            {
+                hero.X += heroSpeed;
+            }
+            if (leftDown == true && hero.X > 0)
+            {
+                hero.X -= heroSpeed;
+            }
+            if (rightDown == true && hero.X > 0)
+            {
+                hero.X += heroSpeed;
+            }
+        
+    
+            //redraw the screen
+            Refresh();
+        }
+
+        private void startButton_click(object sender, EventArgs e)
+        {
+            startButton.Enabled = false;
+            startButton.Visible = false;
+            titleLabel.Text = "";
+            BackColor = Color.Black;
+            
+        }
+    }
+}
+
+
+
